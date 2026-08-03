eda7bc709f40f8f6bdd2aaae945e92a63c754313
harman12-code <harmanearlyintervention@gmail.com>
2026-08-02 22:31:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -123,11 +123,22 @@ function updateDisplay() {
 setInterval(simulateRide,1000);
 function connectBike() {
 
-    document.getElementById("bikeStatus").innerHTML = "Searching...";
+    document.getElementById("bikeStatus").innerHTML =
+        "Searching...";
 
     setTimeout(function(){
 
-        document.getElementById("bikeStatus").innerHTML = "Simulator Mode";
+        document.getElementById("bikeStatus").innerHTML =
+            "Simulator Mode ✅";
+
+        document.getElementById("dataStatus").innerHTML =
+            "Running";
+
+        document.getElementById("powerSource").innerHTML =
+            "Estimated";
+
+        document.getElementById("resistance").innerHTML =
+            "5";
 
     }, 2000);
 
